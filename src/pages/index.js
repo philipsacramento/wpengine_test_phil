bea34d658db510a30890d66a42d22e36d3b6d3a2
philipsacramento <philip.sacramento@gmail.com>
2022-11-03 09:55:44 +0000

    new banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Page() {
   });
   const testimonials = useQuery().testimonials();
   const mainBanner = {
-    sourceUrl: '/static/banner.webp',
+    sourceUrl: '/static/banner.jpg',
     mediaDetails: { width: 1200, height: 600 },
     altText: 'Blog Banner',
   };
